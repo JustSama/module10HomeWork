3c0ab4a2d10fedb03e2693bedf757f673e761afa
JustSama <106009367+JustSama@users.noreply.github.com>
2024-11-08 21:00:55 +0500

    Add files via upload

---
+++ homeWork/composite.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class FileSystemComponent
+{
+    public string Name { get; protected set; }
+    public abstract void Display();
+    public abstract int GetSize();
+}
+
+class File : FileSystemComponent
+{
+    private int size;
+
+    public File(string name, int size)
+    {
+        Name = name;
+        this.size = size;
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"Файл: {Name}, Размер: {size}KB");
+    }
+
+    public override int GetSize()
+    {
+        return size;
+    }
+}
+
+class Directory : FileSystemComponent
+{
+    private List<FileSystemComponent> components = new List<FileSystemComponent>();
+
+    public Directory(string name)
+    {
+        Name = name;
+    }
+
+    public void Add(FileSystemComponent component)
+    {
+        if (!components.Contains(component))
+        {
+            components.Add(component);
+        }
+    }
+
+    public void Remove(FileSystemComponent component)
+    {
+        components.Remove(component);
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"Папка: {Name}");
+        foreach (var component in components)
+        {
+            component.Display();
+        }
+    }
+
+    public override int GetSize()
+    {
+        int totalSize = 0;
+        foreach (var component in components)
+        {
+            totalSize += component.GetSize();
+        }
+        return totalSize;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        File file1 = new File("file1.txt", 100);
+        File file2 = new File("file2.txt", 200);
+        File file3 = new File("file3.txt", 300);
+
+        Directory dir1 = new Directory("Folder1");
+        dir1.Add(file1);
+        dir1.Add(file2);
+
+        Directory dir2 = new Directory("Folder2");
+        dir2.Add(file3);
+
+        Directory root = new Directory("Root");
+        root.Add(dir1);
+        root.Add(dir2);
+
+        root.Display();
+        Console.WriteLine($"Общий размер: {root.GetSize()}KB");
+    }
+}
---
+++ homeWork/fasad.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class TV
+{
+    public void On() => Console.WriteLine("TV включен.");
+    public void Off() => Console.WriteLine("TV выключен.");
+    public void SetChannel(int channel) => Console.WriteLine($"TV: канал установлен на {channel}.");
+}
+
+class AudioSystem
+{
+    public void On() => Console.WriteLine("Аудиосистема включена.");
+    public void Off() => Console.WriteLine("Аудиосистема выключена.");
+    public void SetVolume(int volume) => Console.WriteLine($"Громкость установлена на {volume}.");
+}
+
+class DVDPlayer
+{
+    public void Play() => Console.WriteLine("DVD-проигрыватель: воспроизведение.");
+    public void Pause() => Console.WriteLine("DVD-проигрыватель: пауза.");
+    public void Stop() => Console.WriteLine("DVD-проигрыватель: остановка.");
+}
+
+class GameConsole
+{
+    public void On() => Console.WriteLine("Игровая консоль включена.");
+    public void PlayGame() => Console.WriteLine("Игра запущена на игровой консоли.");
+}
+
+class HomeTheaterFacade
+{
+    private TV tv = new TV();
+    private AudioSystem audio = new AudioSystem();
+    private DVDPlayer dvd = new DVDPlayer();
+    private GameConsole console = new GameConsole();
+
+    public void WatchMovie()
+    {
+        Console.WriteLine("Подготовка к просмотру фильма:");
+        tv.On();
+        audio.On();
+        dvd.Play();
+    }
+
+    public void EndMovie()
+    {
+        Console.WriteLine("Завершение просмотра фильма:");
+        dvd.Stop();
+        tv.Off();
+        audio.Off();
+    }
+
+    public void PlayGame()
+    {
+        Console.WriteLine("Подготовка к запуску игры:");
+        console.On();
+        console.PlayGame();
+    }
+
+    public void ListenToMusic()
+    {
+        Console.WriteLine("Подготовка к прослушиванию музыки:");
+        tv.On();
+        audio.On();
+        audio.SetVolume(50);
+    }
+
+    public void AdjustVolume(int volume)
+    {
+        audio.SetVolume(volume);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade();
+
+        homeTheater.WatchMovie();
+        Console.WriteLine();
+
+        homeTheater.AdjustVolume(30);
+        Console.WriteLine();
+
+        homeTheater.EndMovie();
+        Console.WriteLine();
+
+        homeTheater.PlayGame();
+        Console.WriteLine();
+
+        homeTheater.ListenToMusic();
+        Console.WriteLine();
+    }
+}
